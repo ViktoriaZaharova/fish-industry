bb4cac04e1e729b30cac324f252cb57fe33ce708
viktoriazaharova <zaharova181114@gmail.com>
2020-09-17 23:46:20 +0300

    edit

--- app/js/common.js
+++ app/js/common.js
@@ -44,3 +44,17 @@ $('.btn-close').click(function () {
 
 // animate
 new WOW().init();
+
+$(".form").submit(function () {
+    $.ajax({
+        type: "POST",
+        url: "mail.php",
+        data: $(this).serialize()
+    }).done(function () {
+        $(this).find("input").val("");
+        alert("Спасибо за заявку! Скоро мы с вами свяжемся.");
+
+        $(".form").trigger("reset");
+    });
+    return false;
+});
